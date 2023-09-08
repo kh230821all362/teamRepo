30b25ddbff3240f751c3655388653c34138a6c41
DESKTOP-BLCF8OJ\user1 <kh230821all362@gmail.com>
2023-09-08 10:44:08 +0900

    초기셋팅

---
+++ javaWorkspace/miniGamePrj/src/game/SwyGame.java
@@ -0,0 +1,9 @@
+package game;
+
+public class SwyGame {
+	
+	public void gameStart() {
+		System.out.println("심원용이 만든 게임 시작 ~~~");
+	}
+
+}
---
+++ javaWorkspace/miniGamePrj/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import game.SwyGame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("=====미니게임천국=====");
+		
+		SwyGame swyGame = new SwyGame();
+		swyGame.gameStart();
+		
+	}
+
+}
