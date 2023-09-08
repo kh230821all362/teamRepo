8cedf785421b2196d0be6cc6d4abe4ef2ace9a7f
DESKTOP-4A6NAH4\user1 <msh1411@naver.com>
2023-09-08 12:32:23 +0900

    코드가 삭제되서 재생성..

--- javaWorkspace/miniGamePrj/src/game/Msh5666.java
+++ javaWorkspace/miniGamePrj/src/game/Msh5666.java
@@ -1,12 +1,9 @@
 package game;
 
-public class Msh5666 {
+public class Msh5666{
 	
 	public void gameStart() {
-		System.out.println("나는 배고프다~~~");
+		System.out.println("아니 내 코드가,,,");
 	}
-	
-	
-	
-	
+
 }
